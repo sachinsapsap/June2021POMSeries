0d5ae3bf89588520a5d812d2cf52da6230ea6c84
sachinsapsap <sachinsapsap@gmail.com>
2021-08-30 13:30:04 +0530

    cart feature added

---
+++ src/test/java/com/qa/democart/tests/CartTest.java
@@ -0,0 +1,13 @@
+package com.qa.democart.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CartTest {
+	
+	@Test
+	public void testCart() {
+		Assert.assertEquals(true, true);
+	}
+
+}
